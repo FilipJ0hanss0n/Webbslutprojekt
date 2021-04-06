41cdad021d883a714f04e9ce5d51d48b8f009eed
FilipJ0hanss0n <54435480+FilipJ0hanss0n@users.noreply.github.com>
2021-04-06 14:31:32 +0200

    Skapade grunder och en ikon
    Grunder och en ikon.

---
+++ Slutprojekt/Template/index-template.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="sv">
+	<head>
+		<meta charset="utf-8">
+		<title> Djur </title>
+		<link rel="stylesheet" href="css/stilmall.css">
+		<link rel="shortcut icon" type ="image/png" href="Bild/favicon-32x32.png"/>
+	</head>
+	<body>
+		<?php 
+			require "footer.php";
+		?>
+	</body>
+</html>
